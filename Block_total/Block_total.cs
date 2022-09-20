cb209644813202bf7fc129d4e742c81e824d536d
CreateSouls <Neron1@mail.ru>
2022-09-20 17:38:34 +0300

    Создаем_условие

--- Block_total/Block_total.cs
+++ Block_total/Block_total.cs
@@ -1,2 +1,5 @@
 //Block total
 
+string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
+string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
