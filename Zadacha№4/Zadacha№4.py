40907ab7c8263b372523384162da29c1b50444e9
CreateSouls <Neron1@mail.ru>
2022-10-10 15:04:04 +0300

    Zadacha№4

---
+++ Zadacha№4/Zadacha№4.py
@@ -0,0 +1,12 @@
+#Задача №4
+
+x, y = 4, 6
+ if x > y: big_num = x
+else: big_num = y
+ while(True):
+     if (big_num % x == 0) and (big_num % y == 0):
+         result = big_num
+         break
+     big_num += 1
+ print(result)
+ 
